4487d074f3fcd0d6a02596b5428058147617e624
Jason Hussey <jphussey05@gmail.com>
2025-07-08 14:56:30 -0400

    completes first chapter.

--- settings.py
+++ settings.py
@@ -13,7 +13,8 @@ class Settings:
         self.ship_speed = 2
 
         # Bullet settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 5.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
